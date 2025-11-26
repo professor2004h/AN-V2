29827ee2189a6800453f6d834a490dfb5b27841f
profe <profe@example.com>
2025-11-26 20:56:52 +0530

    Fix ECS service deployment configuration syntax

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -397,10 +397,8 @@ resource "aws_ecs_service" "frontend" {
     container_port   = 3000
   }
 
-  deployment_configuration {
-    maximum_percent         = 200
-    minimum_healthy_percent = 100
-  }
+  deployment_maximum_percent         = 200
+  deployment_minimum_healthy_percent = 100
 
   depends_on = [
     aws_lb_listener.http,
@@ -431,10 +429,8 @@ resource "aws_ecs_service" "backend" {
     container_port   = 3001
   }
 
-  deployment_configuration {
-    maximum_percent         = 200
-    minimum_healthy_percent = 100
-  }
+  deployment_maximum_percent         = 200
+  deployment_minimum_healthy_percent = 100
 
   depends_on = [
     aws_lb_listener.http,
